8ae803c5045f3a0bf0d668f868c7578266e095e4
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2025-03-11 20:17:13 +0100

    Initial monorepo setup

---
+++ apps/frontend/lex-mobile-app/app/(tabs)/index.tsx
@@ -0,0 +1,59 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { useState } from 'react';
+
+export default function HomeScreen() {
+  const [message, setMessage] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://192.168.0.129:3000');
+      const data = await response.json();
+      setMessage(data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  fetchData();
+
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }
+    >
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">{message}</ThemedText>
+        <HelloWave />
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
